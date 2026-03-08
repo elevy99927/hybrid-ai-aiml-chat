f2a9da263acb0ab3d644b3a60a75e349f5e22528
Eyal Levy <eyal@levys.co.il>
2026-03-08 19:16:21 +0200

    Frontend with localhost:3011 URLs

--- port-forward.sh
+++ port-forward.sh
@@ -7,4 +7,5 @@ kubectl port-forward -n lite-llm  svc/lite-helm-litellm  8080:4000 &
 # chatbot
 
 kubectl port-forward -n default  svc/hybrid-bot-frontend  3010:3010 &
+kubectl port-forward -n default  svc/hybrid-bot-backend  3011:3011 &
 
